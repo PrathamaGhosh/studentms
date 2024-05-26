0e82cf49b9f0bdcb44f753cd0d490235b18eec9c
prathama <pgkpa2019@gmail.com>
2024-05-26 20:16:24 +0530

    first commit

---
+++ includes/dbconnection.php
@@ -0,0 +1,19 @@
+<?php 
+// DB credentials.
+define('DB_HOST','localhost:4306');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','studentmsdb');
+// Establish database connection.
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+echo "Successfull";
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
+}
+//$dbh = new PDO("mysql:host="''";dbname=".'studentmsdb','root', '1234',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
+?>
